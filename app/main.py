7608deba321082206f5434efc5ca24b5570948be
Yuxiang LI <yuxiang.li@shipt.com>
2024-08-15 01:04:54 -0400

    initial commit

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
-app.add_middleware(
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 @app.get("/")
 def read_root():
